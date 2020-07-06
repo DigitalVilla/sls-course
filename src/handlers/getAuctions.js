b444104fef5ed06bc20f08cead640caefdfab926
DigitalVilla <digitalvilla01@gmail.com>
2020-07-05 23:13:44 -0600

    Add validation to lamda call

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,14 +1,26 @@
 import AWS from 'aws-sdk';
 import createError from 'http-errors';
+import validator from '@middy/validator';
 import commonWare from './lib/commonWare';
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
+  const { status } = event.queryStringParameters;
   let results = null;
 
   try {
     results = await dynamodb
-      .scan({ TableName: process.env.AUCTIONS_TABLE_NAME })
+      .query({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        IndexName: 'statusClosedDate',
+        KeyConditionExpression: '#status = :status',
+        ExpressionAttributeValues: {
+          ':status': status,
+        },
+        ExpressionAttributeNames: {
+          '#status': 'status',
+        },
+      })
       .promise();
   } catch (error) {
     console.log(error);
@@ -23,4 +35,23 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = commonWare(getAuctions);
+export const handler = commonWare(getAuctions).use(
+  validator({
+    inputSchema: {
+      properties: {
+        queryStringParameters: {
+          type: 'object',
+          properties: {
+            status: {
+              type: 'string',
+              enum: ['OPEN', 'CLOSED'],
+              default: 'OPEN',
+            },
+          },
+        },
+      },
+      required: ['queryStringParameters'],
+    },
+    useDefaults: true,
+  })
+);
